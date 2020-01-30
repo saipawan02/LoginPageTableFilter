076de1a37e0f8d053e7e473e1db188517a3a9ce0
saipawan02 <55680461+saipawan02@users.noreply.github.com>
2020-01-30 20:37:29 +0530

    Add files via upload

---
+++ readFile.js
@@ -0,0 +1,21 @@
+function check(){
+    
+}
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("D:\sem-6\MT\Assignments\MOCK_Data.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
